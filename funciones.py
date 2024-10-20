d28c067ce08daf1b9144b296647bc0ce89ccf42a
German Lopez Pineda <me1lopig@uco.es>
2024-10-20 21:28:04 +0200

    entrada de datos

--- funciones.py
+++ funciones.py
@@ -1,11 +1,75 @@
 # Funciones auxiliares
 
+# crea_directorio->Genera un directorio en el que guardar los cálculos
+# datos_terreno->Importa los datos incluidos en el archivo datos_terreno.xlsx
 # factoresCapacidad->factores de capacidad de carga
 # correcciónForma->Coeficientes de corrección por la forma de la cimentación
 # correccionTalud-> Coeficientes de corrección por cercanía de la cimentación a un talud
 
 
+# Importación de librerías
 import numpy as np
+import openpyxl
+import os
+from datetime import datetime
+
+
+def crea_directorio():
+    # creación del directorio de trabajo para guardar resultados
+    now = datetime.now()
+    directorio=(str(now.year)+str(now.month)+str(now.day)+str(now.hour)+str(now.minute)+str(now.second))
+    os.mkdir(directorio)
+    return directorio
+
+
+def datos_terreno():
+
+    libro = openpyxl.load_workbook('datos_terreno.xlsx')
+    hoja = libro.active
+    
+    # geometría de las capas
+    espesor=[]
+    cotas=[]
+    # valores físicos
+    pe_seco=[]
+    pe_saturado=[]
+    # valores elásticos
+    E=[]
+    poisson=[]
+    tipo_datos=[]
+    # valores de resistencia
+    cohesion=[]
+    fi=[]
+   
+    for row in hoja.iter_rows():
+        espesor.append(row[0].value)
+        espesor[0]=0
+        az=sum(espesor) # vector de espesores
+        nivel_freatico=hoja.cell(row=2, column=2).value
+        pe_seco.append(row[2].value)
+        pe_seco[0]=0
+        pe_saturado.append(row[3].value)
+        pe_saturado[0]=0
+        E.append(row[4].value)
+        E[0]=0
+        poisson.append(row[5].value)
+        poisson[0]=0
+        cohesion.append(row[6].value)
+        cohesion[0]=0
+        fi.append(row[7].value)
+        fi[0]=0
+
+
+
+    for i in np.arange(len(espesor)):
+        cotas.append(sum(espesor[0:i+1]))
+    
+    # se toman los datos de la cabecera
+    for filas in hoja.iter_cols():
+        tipo_datos.append(filas[0].value)
+
+    return espesor,cotas,az,nivel_freatico,pe_seco,pe_saturado,E,poisson,cohesion,fi
+
 
 
 def factoresCapacidad (cohesion,anguloRozamiento):
