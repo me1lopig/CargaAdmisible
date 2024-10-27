003edd4445cd8f55d7877a19ca9de9553410ea29
German Lopez Pineda <me1lopig@uco.es>
2024-10-27 09:24:56 +0100

    funcion de correccion por carga inclnada

--- funciones.py
+++ funciones.py
@@ -5,7 +5,8 @@
 # factoresCapacidad->factores de capacidad de carga
 # correcciónForma->Coeficientes de corrección por la forma de la cimentación
 # correccionTalud-> Coeficientes de corrección por cercanía de la cimentación a un talud
-# densidad-> Cálculo de la densidad del terreno bajo la base de la cimentación
+# correccionInclCarga->Coeficientes de correción por inlinación de la carga
+# densidad-> Cálculo de la densidad del terreno bajo la base de la cimentación con nivel freático
 
 
 # Importación de librerías
@@ -77,7 +78,7 @@ def factoresCapacidad (cohesion,anguloRozamiento):
      
     anguloRozamientoRad=np.deg2rad(anguloRozamiento)
 
-    if (cohesion==0):
+    if (anguloRozamiento==0):
         Nc=(np.pi+2)
         Nq=1
         Ng=0
@@ -114,6 +115,36 @@ def correccionTalud(beta,anguloRozamiento):
         tg=1-np.sin(2*betaRad)
 
     return tc,tq,tg
+
+
+def correcionInclCarga(cargaVertical, horizontalB,horizontalL,fi,cohesion,b,l):
+    
+    # aplicación de la corrección por la inclinación de la carga sobre la cimentación
+    # o lo que es lo mismo por el efecto de la exixtencia de fuerzas horizontales
+    # datos de entrad, cargas sobre cimentación,parámetros del terreno y dimensiones de la cimentación
+    
+    fiRadianes=np.deg2rad(fi) # paso a radianes de fi
+
+    horizontal=np.hypot(horizontalB,horizontalL) # resultante de las fuerzas horizontales
+
+    # tangentes en cada lado de la cimentación
+    tanSigmaL=horizontalL/cargaVertical
+    tanSigmaB=horizontalB/cargaVertical
+
+    nq=factoresCapacidad(cohesion,fi)[1] # se calcula el valor de Nq
+
+    # cálculo de los factores
+    iq=np.power((1-0.7*tanSigmaB),3)*(1-tanSigmaL)
+    ig=(1-tanSigmaB)*(1-tanSigmaL)
+    
+    if fi==0:
+        ic=0.5*(1+np.sqrt(1-horizontal/(b*l*cohesion)))
+    else:
+        ic=(iq*nq-1)/(nq-1)
+
+    return ic,iq,ig
+
+
 
 def densidad(pesoAparente,pesoSaturado,profApoyo,b,zw):
     
