7c6fc23e0ea1b4ad0c42298ebc8d0a619f3013c5
German Lopez Pineda <me1lopig@uco.es>
2024-11-09 12:20:38 +0100

    creacion de nuevas funciones

--- funcionesGuardado.py
+++ funcionesGuardado.py
@@ -1,10 +1,33 @@
 # funciones de guardado de archivo de resultados
+    # guardadoDatos-> Almacena los datos de los cálculos en un archivo txt
+    # guardadoCalculos-> almacena en un archivo txt los resultados de los cálculos 
 
-def guardadoTexto(prof,pesoEspecificoSup,pesoEspecifico,cohesion,anguloRozamiento,Nc,Ng,Nq,sc,sg,sq,tc,tg,tq,ic,ig,iq):
 
-    f=open('calculos.txt','w') # archivo para guardado de los resultados
 
-# envío de datos al archivo de texto
+def guardadoDatos(prof,pesoEspecificoSup,pesoEspecifico,cohesion,anguloRozamiento):
+
+    f=open('calculos.txt','w') # archivo para guardado de los resultados el contenido se reinicia
+
+    # envío de datos al archivo de texto
+    f.write("Profundidad de apoyo de la cimentación %.2f m\n"%(prof))
+    f.write("\n\nParámetros físicos del terreno\n")
+    f.write("peso específico terreno por encima del plano de cimentación %.2f kN/m3 \n"%(pesoEspecificoSup))
+    f.write("peso específico terreno por debajo del plano de cimentación %.2f kN/m3 \n"%(pesoEspecifico))
+    f.write("\n\nParámetros de corte del terreno \n")
+    f.write("Cohesión %.2f kPa\n"%(cohesion))
+    f.write("Fricción %.2f º\n"%(anguloRozamiento))
+    f.write("\n\nResultado de los cálculos\n\n")
+
+    f.close()
+
+
+
+
+def guardadoCalculos(ancho,largo,prof,pesoEspecificoSup,pesoEspecifico,cohesion,anguloRozamiento,Nc,Ng,Nq,sc,sg,sq,tc,tg,tq,ic,ig,iq,qh,qadm):
+
+    f=open('calculos.txt','a') # archivo para guardado de los resultados que ya contiene datos
+
+    # envío de datos al archivo de texto
     f.write("Profundidad de apoyo de la cimentación %.2f m\n"%(prof))
     f.write("\n\nParámetros físicos del terreno\n")
     f.write("peso específico terreno por encima del plano de cimentación %.2f kN/m3 \n"%(pesoEspecificoSup))
